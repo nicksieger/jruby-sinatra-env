4bdf206afd6e6f68228de6f3802ce0f2438a5886
Nick Sieger <nick@nicksieger.com>
2011-05-17 14:36:58 -0400

    Remove JRuby-Rack repo-specific references

--- config/warble.rb
+++ config/warble.rb
@@ -1,14 +1,8 @@
 # Warbler web application assembly configuration file
 Warbler::Config.new do |config|
   config.includes = FileList["appengine-web.xml", "datastore-indexes.xml"]
-
-  if ENV['JRUBY_RACK_SRC']
-    config.java_libs.delete_if {|f| f =~ /jruby-rack[^\/]+\.jar/}
-    config.java_libs += FileList["../../target/jruby-rack*.jar"]
-  end
   config.dirs += ['views']
   require 'socket'
-  config.webxml.ENV_OUTPUT = File.expand_path('../../servers', __FILE__)
   config.webxml.ENV_HOST   = Socket.gethostname
   config.webxml.jruby.rack.ignore.env = true
 end
